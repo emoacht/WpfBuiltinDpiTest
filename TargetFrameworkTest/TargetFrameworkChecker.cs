cf92beb48c7eb53b628108f6597417ca45f95d89
emoacht <emotom@pobox.com>
2016-08-08 06:25:46 +0900

    Added TargetFramework Test

---
+++ TargetFrameworkTest/TargetFrameworkChecker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TargetFrameworkTest
+{
+	public static class TargetFrameworkChecker
+	{
+		public static Version GetTargetFrameworkVersion()
+		{
+			var assembly = Assembly.GetCallingAssembly();
+
+			return GetTargetFrameworkVersion(assembly);
+		}
+
+		public static Version GetTargetFrameworkVersion(Window window)
+		{
+			var assembly = Assembly.GetAssembly(window.GetType());
+
+			return GetTargetFrameworkVersion(assembly);
+		}
+
+		public static Version GetTargetFrameworkVersion(Assembly assembly)
+		{
+			var atr = assembly?.GetCustomAttributes(typeof(TargetFrameworkAttribute), false);
+
+			var tfv = atr?.FirstOrDefault() as TargetFrameworkAttribute;
+			if (tfv == null)
+				return null;
+
+			var pattern = new Regex(@"Version=v(?<version>\d\.\d\.\d)$");
+			var match = pattern.Match(tfv.FrameworkName);
+			if (!match.Success)
+				return null;
+
+			return new Version(match.Groups["version"].Value);
+		}
+	}
+}
